69574be38e717d3e4d249c1b47cf32f7df66aae8
apple <842539314@qq.com>
2018-06-12 17:20:09 +0800

    5666

--- app/src/main/java/cn/edu/gdmec/android/demo21/liereader/News/Model/NewsModel.java
+++ app/src/main/java/cn/edu/gdmec/android/demo21/liereader/News/Model/NewsModel.java
@@ -20,22 +20,23 @@ public class NewsModel implements INewsModel{
     public void loadNews( final String hostType,  final int startPage,
                           final String id,  final IOnLoadListener iOnLoadListener) {
         RetrofitHelper retrofitHelper=new RetrofitHelper(Api.NEWS_HOST);
-        retrofitHelper.getNews(hostType,id,startPage).enqueue(new Callback<NewBean>() {
-            @Override
-            public void onResponse(Call<NewBean> call, Response<NewBean> response) {
-                Log.i("onResponse", "onResponse: "+response.body());
-                if (response.isSuccessful()){
-                    iOnLoadListener.success(response.body());
-                }else {
-                    iOnLoadListener.fail("");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<NewBean> call, Throwable t) {
-             iOnLoadListener.fail(t.toString());
-            }
-        });
+//        retrofitHelper.getNews(hostType,id,startPage).enqueue(new Callback<NewBean>() {
+//            @Override
+//            public void onResponse(Call<NewBean> call, Response<NewBean> response) {
+//                Log.i("onResponse", "onResponse: "+response.body());
+//                if (response.isSuccessful()){
+//                    iOnLoadListener.success(response.body());
+//
+//                }else {
+//                    iOnLoadListener.fail("");
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<NewBean> call, Throwable t) {
+//             iOnLoadListener.fail(t.toString());
+//            }
+//        });
 
     }
 }
